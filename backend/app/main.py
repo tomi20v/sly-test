4e17ec70af4589a07defbcff431c05b20a869fc5
temesvari.tamas <temesvari.tamas@gmail.com>
2025-12-08 20:22:26 +0000

    added error handling to startup and to endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,25 +9,35 @@ from app.config import Config
 app = Flask(__name__)
 api = Api(app)
 
-@app.route("/")
-def hello_world():
-  """Example Hello World route."""
-  name = os.environ.get("NAME", "World")
-  return f"Hello {name}!"
+try:
+  # Database configuration
+  config = Config()
 
-# Database configuration
-config = Config()
+  # Create a database instance
+  db = Database(host=config.MYSQL_HOST, user=config.MYSQL_USER, password=config.MYSQL_PASSWORD, database=config.MYSQL_DATABASE)
 
-# Create a database instance
-db = Database(host=config.MYSQL_HOST, user=config.MYSQL_USER, password=config.MYSQL_PASSWORD, database=config.MYSQL_DATABASE)
+  @app.route("/")
+  def hello_world():
+    """Example Hello World route."""
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
 
-# Inject the database instance into the Items resource
-api.add_resource(Items, '/api/items', resource_class_kwargs={'db': db})
+  # Inject the database instance into the Items resource
+  api.add_resource(Items, '/api/items', resource_class_kwargs={'db': db})
 
-# Register a function to close the database connection when the app exits
-@app.teardown_appcontext
-def teardown_db(exception=None):
-    db.close()
+  # Register a function to close the database connection when the app exits 
+  # (I'd have to check if it's really a good practice like this?)
+  @app.teardown_appcontext
+  def teardown_db(exception=None):
+      if db:
+          db.close()
+
+except mysql.connector.Error as e:
+  print("Database connection failed.")
+  exit(1)
+except Exception as e:
+  print(e)
+  exit(1)
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
