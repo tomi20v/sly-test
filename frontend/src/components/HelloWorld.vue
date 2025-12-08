17f43f0f70c734fc465b1b15c721f9e033f8775d
temesvari.tamas <temesvari.tamas@gmail.com>
2025-12-08 22:17:21 +0000

    frontend - added Vuetify

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,41 +1,41 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+  import { ref } from 'vue'
+  
+  defineProps<{ msg: string }>()
+  
+  const count = ref(0)
+  </script>
+  
+  <template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify {{ msg }}
+        </h1>
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
-<template>
-    <h1>{{ msg }}</h1>
-
-    <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-        Edit
-        <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-    </div>
-
-    <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-        >create-vue</a
-    >, the official Vue + Vite starter
-    </p>
-    <p>
-    Learn more about IDE Support for Vue in the
-    <a
-        href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-        target="_blank"
-        >Vue Docs Scaling up Guide</a
-    >.
-    </p>
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
+        </p>
+        <v-btn
+          class="text-none mb-4"
+          color="indigo-darken-3"
+          size="x-large"
+          variant="flat"
+          block
+          prepend-icon="mdi-truck"
+          @click="count++"
+        >click me {{ count }}</v-btn>
+        <v-alert text="lorem ipsum dolor sit amet" title="alert title" type="success"></v-alert>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<style scoped>
-.read-the-docs {
-    color: #888;
-}
-</style>
+<script setup lang="ts">
+</script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,4 +2,19 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
+})
+
+createApp(App).use(vuetify).mount('#app')
