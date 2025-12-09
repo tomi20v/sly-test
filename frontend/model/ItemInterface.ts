797ebd3dd8b21c93897d37de51f5daee25b5ff5c
temesvari.tamas <temesvari.tamas@gmail.com>
2025-12-09 08:28:29 +0000

    prototype for calling our own backend (dev mode only)

---
+++ frontend/model/ItemInterface.ts
@@ -0,0 +1,6 @@
+export interface ItemInterface {
+    id: number;
+    name: string;
+    description: string | null;
+    price: number;
+}
--- frontend/src/store/app.ts
+++ frontend/src/store/app.ts
@@ -1,12 +1,23 @@
 import { defineStore } from 'pinia'
+import type { ItemInterface } from '../../model/ItemInterface';
+import axios from 'axios';
 
 export const useAppStore = defineStore('app', {
   state: () => ({
     count: 0,
+    items: [] as ItemInterface[],
   }),
   actions: {
     increment() {
       this.count++
+    },
+    async fetchItems() {
+      try {
+        const response = await axios.get(`/api/items`);
+        this.items = response.data;
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
     },
   },
 })
