f06573031933bf56d126ffc4a69ca1dfb9f64792
temesvari.tamas <temesvari.tamas@gmail.com>
2025-12-09 23:21:03 +0000

    product list looks nicer, loads from backend with load bar

--- frontend/src/store/itemStore.ts
+++ frontend/src/store/itemStore.ts
@@ -2,21 +2,25 @@ import { defineStore } from 'pinia'
 import type { ItemInterface } from '../../model/ItemInterface';
 import axios from 'axios';
 
-export const useAppStore = defineStore('app', {
+export const useItemStore = defineStore('items', {
   state: () => ({
     count: 0,
     items: [] as ItemInterface[],
+    itemsLoading: false,
   }),
   actions: {
     increment() {
       this.count++
     },
     async fetchItems() {
+      this.itemsLoading = true;
       try {
         const response = await axios.get(`/api/items`);
         this.items = response.data;
       } catch (error) {
         console.error('Error fetching items:', error);
+      } finally {
+        this.itemsLoading = false;
       }
     },
   },
