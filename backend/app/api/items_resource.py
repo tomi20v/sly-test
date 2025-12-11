3bfd172874d243c504b9b5cf425b8f9ddf0f5d5e
temesvari.tamas <temesvari.tamas@gmail.com>
2025-12-11 18:54:20 +0100

    purchase polling - fixed that converting for serialization should happen at the right place (ie in the mapped resource, not in the repository)

--- backend/app/api/items_resource.py
+++ backend/app/api/items_resource.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource
+from app.helpers import to_json_serializable, to_json_serializable_array
 
 class ItemsResource(Resource):
   def __init__(self, **kwargs):
@@ -7,7 +8,7 @@ class ItemsResource(Resource):
   def get(self):
       try:
           items = self.items_repository.get_all()
-          return items, 200
+          return to_json_serializable_array(items), 200
       except Exception as e:
           # NOTE: It's good practice to log the error e for debugging
           return {"error": "An unexpected error occurred"}, 500
