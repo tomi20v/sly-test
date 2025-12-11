1cf3bd8c7fda365bfb33e599bc3ffefff2186047
temesvari.tamas <temesvari.tamas@gmail.com>
2025-12-11 21:48:45 +0000

    tidy - abstracted PurchaseStore to load purchases, load a purchase (while polling), and creating a purchase

--- frontend/src/components/PurchaseListDialog.vue
+++ frontend/src/components/PurchaseListDialog.vue
@@ -8,23 +8,23 @@
       </v-card-title>
       <v-card-text>
         <v-progress-linear
-          v-if="state === 'loading' || itemStore.itemsLoading"
+          v-if="purchaseStore.purchasesLoading || itemStore.itemsLoading"
           indeterminate
           color="primary"
           class="mb-4"
         ></v-progress-linear>
 
-        <v-alert v-else-if="state === 'error'" type="error" dense class="mb-4">
-          {{ errorMessage }}
+        <v-alert v-else-if="purchaseStore.purchasesError" type="error" dense class="mb-4">
+          {{ purchaseStore.purchasesError }}
         </v-alert>
 
-        <div v-else-if="state === 'empty'" class="text-center">
+        <div v-else-if="purchaseStore.purchases.length === 0" class="text-center">
           You have not purchased anything.... Yet?
         </div>
 
-        <v-list v-else-if="state === 'loaded'" lines="one" dense>
+        <v-list v-else lines="one" dense>
           <v-list-item
-            v-for="purchase in purchases"
+            v-for="purchase in purchaseStore.purchases"
             :key="purchase.id"
             :title="getItemName(purchase.item_id)"
             :subtitle="`Time: ${new Date(purchase.created_at).toLocaleString()} ${purchase.payment_status.toUpperCase()}`"
@@ -37,10 +37,10 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
+import { computed, watch } from 'vue';
 import CloseButton from './CloseButton.vue';
 import { useItemStore } from '../store/itemStore';
-import { type PurchaseInterface } from '../model/PurchaseInterface';
+import { usePurchaseStore } from '../store/purchaseStore';
 
 const props = defineProps<{
   modelValue: boolean;
@@ -51,6 +51,7 @@ const emit = defineEmits<{
 }>();
 
 const itemStore = useItemStore();
+const purchaseStore = usePurchaseStore();
 
 const showDialog = computed({
   get() {
@@ -61,36 +62,9 @@ const showDialog = computed({
   }
 });
 
-const purchases = ref<PurchaseInterface[]>([]);
-const state = ref('loading'); // loading | error | empty | loaded
-const errorMessage = ref('');
-
-async function fetchPurchases() {
-  state.value = 'loading';
-  try {
-    // Assuming user_id=1 for now.
-    const response = await fetch('/api/users/2/purchases');
-    const data = await response.json();
-
-    if (!response.ok) {
-      throw new Error(data.error || 'Failed to fetch purchases');
-    }
-    
-    if (data.length === 0) {
-      state.value = 'empty';
-    } else {
-      purchases.value = data;
-      state.value = 'loaded';
-    }
-  } catch (error: any) {
-    errorMessage.value = error.message;
-    state.value = 'error';
-  }
-}
-
 watch(() => props.modelValue, (newValue) => {
   if (newValue) {
-    fetchPurchases();
+    purchaseStore.fetchPurchases();
     itemStore.fetchItems();
   }
 });
